cc87990bf77455ef85f7e54cd4541f5e2f274592
Old <225056366+roy-henderson@users.noreply.github.com>
2026-05-20 14:44:21 -0400

    build: Javari Market Forge

--- next.config.js
+++ next.config.js
@@ -1,17 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ['images.unsplash.com', 'source.unsplash.com', 'via.placeholder.com'],
-    unoptimized: true,
-  },
-}
-
-module.exports = nextConfig
+/** @type {import("next").NextConfig} */
+const nextConfig={typescript:{ignoreBuildErrors:true},eslint:{ignoreDuringBuilds:true}}
+module.exports=nextConfig
